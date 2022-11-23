74f705a80fb40150ba303059776434362412434d
Felix Chan <felixc63@nycstudents.net>
2022-11-22 23:49:14 -0500

    Working...

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,193 @@
-package PACKAGE_NAME;public class TicTacToe {
+public class TicTacToe {
+    private int botScore;
+    private int userScore;
+    private String table;
+    private String indexOne;
+    private String indexTwo;
+    private String indexThree;
+    private String indexFour;
+    private String indexFive;
+    private String indexSix;
+    private String indexSeven;
+    private String indexEight;
+    private String indexNine;
+    private String symbol;
+    public TicTacToe(String symbol){
+        botScore = 0;
+        userScore = 0;
+        table = "";
+        indexOne = "";
+        indexTwo = "";
+        indexThree = "";
+        indexFour = "";
+        indexFive = "";
+        indexSix = "";
+        indexSeven = "";
+        indexEight = "";
+        indexNine = "";
+        this.symbol = symbol;
+    }
+
+    public TicTacToe(String symbol, int repeat){
+        botScore = 0;
+        userScore = 0;
+        table = "";
+        indexOne = "";
+        indexTwo = "";
+        indexThree = "";
+        indexFour = "";
+        indexFive = "";
+        indexSix = "";
+        indexSeven = "";
+        indexEight = "";
+        indexNine = "";
+        this.symbol = symbol;
+    }
+    public int getBotScore(){
+        return botScore;
+    }
+
+    public int getUserScore(){
+        return userScore;
+    }
+
+    public String getTable(){
+        return table;
+    }
+    public String printTable(String one, String two, String three, String four, String five, String six, String seven, String eight, String nine){
+        String table = ("|---|---|---|") + "\n";
+        table += ("| " + one + " | " + two + " | " + three + " |") + "\n";
+        table += ("|-----------|") + "\n";
+        table += ("| " + four + " | " + five + " | " + six + " |") + "\n";
+        table += ("|-----------|") + "\n";
+        table += ("| " + seven + " | " + eight + " | " + nine + " |") + "\n";
+        table += ("|---|---|---|") + "\n";
+
+        indexOne = table.substring(table.indexOf(one), table.indexOf(one)+1);
+        indexTwo = table.substring(table.indexOf(two), table.indexOf(two)+1);
+        indexThree = table.substring(table.indexOf(three), table.indexOf(three)+1);
+        indexFour = table.substring(table.indexOf(four), table.indexOf(four)+1);
+        indexFive = table.substring(table.indexOf(five), table.indexOf(five)+1);
+        indexSix = table.substring(table.indexOf(six), table.indexOf(six)+1);
+        indexSeven = table.substring(table.indexOf(seven), table.indexOf(seven)+1);
+        indexEight = table.substring(table.indexOf(eight), table.indexOf(eight)+1);
+        indexNine = table.substring(table.indexOf(nine), table.indexOf(nine)+1);
+
+        this.table = table;
+
+        return table;
+    }
+
+    public void calcSpot(String spot, String symbol){
+        if(spot.equals("1")){
+            System.out.println(printTable(symbol,indexTwo,indexThree,indexFour,indexFive,indexSix,indexSeven,indexEight,indexNine));
+        }else if(spot.equals("2")){
+            System.out.println(printTable(indexOne,symbol,indexThree,indexFour,indexFive,indexSix,indexSeven,indexEight,indexNine));
+        }else if(spot.equals("3")){
+            System.out.println(printTable(indexOne,indexTwo,symbol,indexFour,indexFive,indexSix,indexSeven,indexEight,indexNine));
+        }else if(spot.equals("4")){
+            System.out.println(printTable(indexOne,indexTwo,indexThree,symbol,indexFive,indexSix,indexSeven,indexEight,indexNine));
+        }else if(spot.equals("5")){
+            System.out.println(printTable(indexOne,indexTwo,indexThree,indexFour,symbol,indexSix,indexSeven,indexEight,indexNine));
+        }else if(spot.equals("6")){
+            System.out.println(printTable(indexOne,indexTwo,indexThree,indexFour,indexFive,symbol,indexSeven,indexEight,indexNine));
+        }else if(spot.equals("7")){
+            System.out.println(printTable(indexOne,indexTwo,indexThree,indexFour,indexFive,indexSix,symbol,indexEight,indexNine));
+        }else if(spot.equals("8")){
+            System.out.println(printTable(indexOne,indexTwo,indexThree,indexFour,indexFive,indexSix,indexSeven,symbol,indexNine));
+        }else if(spot.equals("9")){
+            System.out.println(printTable(indexOne,indexTwo,indexThree,indexFour,indexFive,indexSix,indexSeven,indexEight,symbol));
+        }
+    }
+    public void userPlay(String spot, String symbol){
+        System.out.println("Your move: ");
+        calcSpot(spot,symbol); //User Move
+
+    }
+
+    public void botPlay(){
+        Integer randNum = 0;
+        while (true) { //Repeats randomizer until the bot finds an empty spot
+            randNum = (int) (Math.random() * 9 + 1);
+            if (getTable().contains(randNum.toString())){
+                break;
+            }
+        }
+        System.out.println("Bot's move:");
+        calcSpot(randNum.toString(), "x"); //Bot Move
+    }
+
+    public boolean checkWin(){ //Checks every winning possibility in TicTacToe
+        //Horizontal wins
+        if (indexOne.equals(indexTwo) && indexOne.equals(indexThree)){
+            if (indexOne.equals(symbol)){
+                userScore++;
+            }else{
+                botScore++;
+            }
+            return true;
+        }
+        if (indexFour.equals(indexFive) && indexFour.equals(indexSix)){
+            if (indexFour.equals(symbol)){
+                userScore++;
+            }else{
+                botScore++;
+            }
+            return true;
+        }
+        if (indexSeven.equals(indexEight) && indexSeven.equals(indexNine)){
+            if (indexSeven.equals(symbol)){
+                userScore++;
+            }else{
+                botScore++;
+            }
+            return true;
+        }
+
+        //Vertical wins
+        if (indexOne.equals(indexFour) && indexOne.equals(indexSeven)){
+            if (indexOne.equals(symbol)){
+                userScore++;
+            }else{
+                botScore++;
+            }
+            return true;
+        }
+        if (indexTwo.equals(indexFive) && indexTwo.equals(indexEight)){
+            if (indexTwo.equals(symbol)){
+                userScore++;
+            }else{
+                botScore++;
+            }
+            return true;
+        }
+        if (indexThree.equals(indexSix) && indexThree.equals(indexNine)){
+            if (indexThree.equals(symbol)){
+                userScore++;
+            }else{
+                botScore++;
+            }
+            return true;
+        }
+
+        //Diagonal wins
+        if (indexOne.equals(indexFive) && indexOne.equals(indexNine)){
+            if (indexOne.equals(symbol)){
+                userScore++;
+            }else{
+                botScore++;
+            }
+            return true;
+        }
+        if (indexThree.equals(indexFive) && indexFour.equals(indexSeven)){
+            if (indexThree.equals(symbol)){
+                userScore++;
+            }else{
+                botScore++;
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- src/TicTacToeRunner.java
+++ src/TicTacToeRunner.java
@@ -1,10 +1,29 @@
 import java.util.Scanner;
 
-public class Main {
+public class TicTacToeRunner {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        TicTacToe ticTacToe = new TicTacToe("");
 
         System.out.println("Welcome to TicTacToe!");
+        System.out.println("You will be versing a bot.");
+
+        String symbol = "o";
+        while (true) {
+            System.out.print("Choose your desired symbol (Note: bot uses symbol 'x' already): ");
+            symbol = scan.nextLine();
+            if (symbol.length() > 1){
+                System.out.println("Your symbol length is too big.");
+                System.out.println("Type only one character.");
+            }else if (symbol.equals("1") || symbol.equals("2") || symbol.equals("3") || symbol.equals("4") || symbol.equals("5") || symbol.equals("6") || symbol.equals("7") || symbol.equals("8") || symbol.equals("9")){
+                System.out.println("Your symbol cannot be a number.");
+            }else if (symbol.equalsIgnoreCase("x")){
+                System.out.println("The bot uses symbol 'x' already.");
+                System.out.println("Try again.");
+            }else{
+                break;
+            }
+        }
 
         boolean isIntGreaterThanOne = true;
         int repeat = 0;
@@ -20,14 +39,73 @@ public class Main {
             }
         }
 
-        System.out.println("Creating an empty table...");
-
         if (repeat == 1){
-            TicTacToe ticTacToe = new TicTacToe();
+            ticTacToe = new TicTacToe(symbol);
         }else{
-            TicTacToe ticTacToe = new TicTacToe(repeat);
+            ticTacToe = new TicTacToe(symbol,repeat);
         }
 
+        for(int i = 0; i < repeat; i++) { //Loop repeats until it reaches user's repeat input
+            System.out.println("Creating an empty table...");
+            System.out.print(ticTacToe.printTable("1", "2", "3", "4", "5", "6", "7", "8", "9"));
 
+            String spot = "";
+            while(true) {
+                while (true) {
+                    System.out.print("Input a number from the table for your move: ");
+                    spot = scan.next();
+                    if (!(ticTacToe.getTable().contains(spot))) {
+                        System.out.println("Please input a number that's on the table.");
+                        System.out.println("Try again.");
+                    } else {
+                        break;
+                    }
+                }
+
+                ticTacToe.userPlay(spot, symbol);
+                if (ticTacToe.checkWin()){
+                    break;
+                }
+                ticTacToe.botPlay();
+
+                //if (!(ticTacToe.getTable().contains("1")) && !(ticTacToe.getTable().contains("2")) && !(ticTacToe.getTable().contains("3")) && !(ticTacToe.getTable().contains("4")) && !(ticTacToe.getTable().contains("5")) && !(ticTacToe.getTable().contains("6")) && !(ticTacToe.getTable().contains("7")) && !(ticTacToe.getTable().contains("8")) &&  !(ticTacToe.getTable().contains("9"))){ //Loop stops when the table is full
+                //}
+                if (ticTacToe.checkWin()){
+                    break;
+                }
+            }
+            System.out.println("Current Standings: " + ticTacToe.getUserScore()  + "-" + ticTacToe.getBotScore());
+        }
+
+        int userScore = ticTacToe.getUserScore();
+        int botScore = ticTacToe.getBotScore();
+        int difference = Math.abs(userScore - botScore);
+        String plural = "";
+        System.out.println("Final Results: " + userScore + "-" + botScore);
+        if (userScore > botScore){
+            System.out.println("You win!");
+            if (difference > 1){
+                plural = "s";
+            }
+            System.out.println("You beat the bot by " + difference +" point" + plural + "!");
+            System.out.println("Great job!");
+        }else if(userScore < botScore){
+            System.out.println("The bot wins!");
+            if (difference > 1){
+                plural = "s";
+            }
+            System.out.println("The bot beats you by " + difference +" point" + plural + "!");
+            System.out.println("Better luck next time!");
+        }else{
+            System.out.println("Tie!");
+            System.out.println("What a fierce battle!");
+        }
+
+        System.out.println();
+        if (repeat == 1){
+            System.out.println("Thank you for playing TicTacToe!");
+        }else {
+            System.out.println("Thank you for playing TicTacToe " + repeat + " times!");
+        }
     }
 }
